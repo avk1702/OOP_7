b8a2928f3e124b9ecd685337bfc2219eeb71c1e0
avk1702 <avk1702@bk.ru>
2024-02-12 12:45:54 +0500

    4

---
+++ ComplexCalculatorImpl.java
@@ -0,0 +1,24 @@
+package OOP_7;
+
+public class ComplexCalculatorImpl implements ComplexCalculator {
+
+    @Override
+    public ComplexNumber add(ComplexNumber num1, ComplexNumber num2) {
+        
+        return null;
+    }
+
+    @Override
+    public ComplexNumber divide(ComplexNumber num1, ComplexNumber num2) {
+       
+        return null;
+    }
+
+    @Override
+    public ComplexNumber multiply(ComplexNumber num1, ComplexNumber num2) {
+        
+        return null;
+    }
+    
+    
+}
