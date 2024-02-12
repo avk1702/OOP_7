086707715111d53063836b0c1f8f70b23c050aac
avk1702 <avk1702@bk.ru>
2024-02-12 12:46:32 +0500

    5

---
+++ ComplexNumber.java
@@ -0,0 +1,20 @@
+package OOP_7;
+
+public class ComplexNumber {
+    private double real;
+    private double imaginary;
+    public double getReal() {
+        return real;
+    }
+    public void setReal(double real) {
+        this.real = real;
+    }
+    public double getImaginary() {
+        return imaginary;
+    }
+    public void setImaginary(double imaginary) {
+        this.imaginary = imaginary;
+    }
+
+    
+}
