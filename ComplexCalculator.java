0d342f200a45209c997453375bc71380988ac2cb
avk1702 <avk1702@bk.ru>
2024-02-12 12:42:32 +0500

    1

---
+++ ComplexCalculator.java
@@ -0,0 +1,7 @@
+package OOP_7;
+
+public interface ComplexCalculator {
+    ComplexNumber add(ComplexNumber num1, ComplexNumber num2);
+    ComplexNumber multiply(ComplexNumber num1, ComplexNumber num2);
+    ComplexNumber divide(ComplexNumber num1, ComplexNumber num2);
+}
